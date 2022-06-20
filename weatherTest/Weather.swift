80427270a0969b4582b5cee020dcba54d94a327f
Tassja <Hannatassja@student.umn.ac.id>
2022-06-20 09:04:30 +0700

    Get data

--- weatherTest/Weather.swift
+++ weatherTest/Weather.swift
@@ -7,17 +7,64 @@
 
 import Foundation
 
-protocol Info {
-    
+struct Weather: Decodable {
+    var lat: Double
+    var lon: Double
+    var current: CurrentWeather
+    var daily: [DailyWeather]
 }
 
-class Weather {
-    var lat: String
-    var lon: String
+struct CurrentWeather: Decodable {
+    var dt: Double
+    var temp: Double
+    var pressure: Double
+    var humidity: Double
+    var wind_speed: Double
+    var weather: [WeatherDetail]
+}
+
+struct WeatherDetail: Decodable {
+    var main: String
+    var icon: String
+}
+
+struct DailyWeather: Decodable {
+    var dt: Double
+    var temp: Temp
+    var weather: [WeatherDetail]
+}
+
+struct Temp: Decodable {
+    var day: Double
+}
+
+struct data {
+    var country: String
+    var cities: [String]
     
-    init(lat: String, lon: String){
-        self.lat = lat
-        self.lon = lon
+    enum CodingKeys: String, CodingKey {
+        case country, cities
     }
 }
 
+extension data: Decodable {
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        country = try values.decode(String.self, forKey: .country)
+        cities = try values.decode([String].self, forKey: .cities)
+    }
+}
+
+extension data: Encodable {
+    func encode(to encoder: Encoder) throws {
+        var container  = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(self.country, forKey: .country)
+        try container.encode(self.cities, forKey: .cities)
+    }
+}
+
+struct test: Decodable {
+    var lat: Double
+    var lon: Double
+}
--- weatherTest/WeatherListVC.swift
+++ weatherTest/WeatherListVC.swift
@@ -6,27 +6,125 @@
 //
 
 import UIKit
+import Alamofire
+import MapKit
+import CoreLocation
 
-class WeatherListVC: UIViewController {
+class WeatherListVC: UIViewController, CLLocationManagerDelegate {
     
     private let weatherTableView = UITableView()
-    private let search = UISearchController(searchResultsController: nil)
+    
+    private var weatherSearchTableVC = WeatherSearchTableVC()
     
     var safeArea: UILayoutGuide!
     
-    var animal = ["cat", "dog", "woman"]
+    var locManager = CLLocationManager()
+    var currLocation = CLLocation()
+    
+    var weatherList = [Weather]()
+    
+    var animal = ["animal", "test"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        //fetchData()
         
         setupView()
+        fetchLocation()
+        
         createSubview()
         addConstraints()
+        
+        print("test 2", weatherList.count)
+    }
+    
+//    private func fetchYourLocation(){
+//        locManager.delegate = self
+//
+//        locManager.requestAlwaysAuthorization()
+//        locManager.requestWhenInUseAuthorization()
+//
+//        locManager.startUpdatingLocation()
+//
+//        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+//            guard let currLocation = locManager.location else {
+//                return
+//            }
+//
+//            let req = AF.request("https://api.openweathermap.org/data/2.5/onecall?lat=\(currLocation.coordinate.latitude)&lon=\(currLocation.coordinate.longitude)&exclude=minutely,hourly&appid=5d10f44082fed78e8349fc49ee313386").validate()
+//
+//            req.responseDecodable(of: Weather.self) { (response) in
+//                guard let test = response.value else {return}
+//                self.weatherList.append(test)
+//                //print(test)
+//                print("test")
+//            }
+//        }
+//    }
+    
+    private func fetchLocation() {
+        
+        var url: URL!
+
+        locManager.delegate = self
+
+        locManager.requestAlwaysAuthorization()
+        locManager.requestWhenInUseAuthorization()
+
+        locManager.startUpdatingLocation()
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            guard let currLocation = locManager.location else {
+                return
+            }
+
+            url = URL(string: "https://api.openweathermap.org/data/2.5/onecall?lat=\(currLocation.coordinate.latitude)&lon=\(currLocation.coordinate.longitude)&exclude=minutely,hourly&appid=5d10f44082fed78e8349fc49ee313386")!
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                do {
+                    let res = try JSONDecoder().decode(Weather.self, from: data)
+                    DispatchQueue.main.async{
+                        self.weatherList.append(res)
+            
+                        self.weatherTableView.reloadData()
+                    }
+                    print("res", res.lat)
+                } catch let error {
+                    print("error", error)
+                }
+            }
+        }
+        task.resume()
+        
+        
+        print("test 3", weatherList.count)
+    }
+    
+    private func fetchData(){
+        
+        let req = AF.request("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely,hourly&appid=5d10f44082fed78e8349fc49ee313386").validate()
+        
+        req.responseDecodable(of: Weather.self) { (response) in
+            
+            guard let test = response.value else {return}
+            print(test.daily)
+            
+        }
+
     }
     
     private func setupView(){
+        
+        let search = UISearchController(searchResultsController: weatherSearchTableVC)
+        
+        search.searchResultsUpdater = weatherSearchTableVC
         self.navigationItem.searchController = search
         self.navigationItem.hidesSearchBarWhenScrolling = false
+        
+        definesPresentationContext = true
         
         weatherTableView.delegate = self
         weatherTableView.dataSource = self
@@ -60,7 +158,13 @@ class WeatherListVC: UIViewController {
 
 extension WeatherListVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return animal.count
+        if weatherList.count == 0 {
+            
+        } else {
+            self.weatherTableView.reloadData()
+        }
+        
+        return weatherList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
